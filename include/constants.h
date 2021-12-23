00ee4cf42e72eba36b08331405341f6e9baed835
Leonardo Montagner <leomonta2002@gmail.com>
2021-12-23 11:36:54 +0100

    added rotated tetraminos

--- include/constants.h
+++ include/constants.h
@@ -19,49 +19,141 @@
 
 /**
  * # # # # *
+ * 
+ * # /
+ * # /
+ * # /
+ * # *
  */
-const wchar_t tetr_I[5] = {L'▣', L'▣', L'▣', L'▣', L'*'};
+const wchar_t tetr_I_0[5] = {L'▣', L'▣', L'▣', L'▣', L'*'};
+
+const wchar_t tetr_I_1[8] = {L'▣', L'/',
+							 L'▣', L'/',
+							 L'▣', L'/',
+							 L'▣', L'*'};
 
 /**
- * _ # _ /
+ * _ # /
  * # # # *
+ * 
+ * # /
+ * # # / 
+ * # *
+ * 
+ * # # # /
+ * _ # *
+ * 
+ * _ # /
+ * # # /
+ * _ # *
  */
-const wchar_t tetr_T[8] = {L'_', L'■', L'_', L'/',
-						   L'■', L'■', L'■', L'*'};
+const wchar_t tetr_T_0[7] = {L'_', L'■', L'/',
+							 L'■', L'■', L'■', L'*'};
+
+const wchar_t tetr_T_1[7] = {L'■', L'/',
+							 L'■', L'■', L'/',
+							 L'■', L'*'};
+
+const wchar_t tetr_T_2[7] = {L'■', L'■', L'■', L'/',
+							 L'_', L'■', L'*'};
+
+const wchar_t tetr_T_3[9] = {L'_', L'■', L'/',
+							 L'■', L'■', L'/',
+							 L'_', L'■', L'*'};
 
 /**
  * # # /
  * # # *
  */
-const wchar_t tetr_Q[6] = {L'◲', L'◱', L'/',
-						   L'◳', L'◰', L'*'};
+const wchar_t tetr_Q_0[6] = {L'◲', L'◱', L'/',
+							 L'◳', L'◰', L'*'};
 
 /**
  * # # # /
- * # _ _ *
+ * # *
+ * 
+ * # # /
+ * _ # /
+ * _ # *
+ * 
+ * _ _ # /
+ * # # # *
+ * 
+ * # /
+ * # /
+ * # # *
  */
-const wchar_t tetr_L[8] = {L'□', L'□', L'□', L'/',
-						   L'□', L'_', L'_', L'*'};
+const wchar_t tetr_L_0[6] = {L'□', L'□', L'□', L'/',
+							 L'□', L'*'};
+
+const wchar_t tetr_L_1[9] = {L'□', L'□', L'/',
+							 L'_', L'□', L'/',
+							 L'_', L'□', L'*'};
+
+const wchar_t tetr_L_2[8] = {L'_', L'_', L'□', L'/',
+							 L'□', L'□', L'□', L'*'};
+
+const wchar_t tetr_L_3[9] = {L'_', L'□', L'/',
+							 L'_', L'□', L'/',
+							 L'□', L'□', L'*'};
 
 /**
  * # # # /
  * _ _ # *
+ * 
+ * _ # /
+ * _ # /
+ * # # *
+ * 
+ * # /
+ * # # # *
+ * 
+ * # # /
+ * # /
+ * # *
  */
-const wchar_t tetr_J[8] = {L'⬚', L'⬚', L'⬚', L'/',
-						   L'_', L'_', L'⬚', L'*'};
+const wchar_t tetr_J_0[8] = {L'⬚', L'⬚', L'⬚', L'/',
+							 L'_', L'_', L'⬚', L'*'};
+
+const wchar_t tetr_J_1[9] = {L'_', L'⬚', L'/',
+							 L'_', L'⬚', L'/',
+							 L'⬚', L'⬚', L'*'};
+
+const wchar_t tetr_J_2[6] = {L'⬚', L'/',
+							 L'⬚', L'⬚', L'⬚', L'*'};
+
+const wchar_t tetr_J_3[7] = {L'⬚', L'⬚', L'/',
+							 L'⬚', L'/',
+							 L'⬚', L'*'};
 
 /**
- * # # _ /
+ * # # /
  * _ # # *
+ * 
+ * _ # /
+ * # # /
+ * # *
  */
-const wchar_t tetr_Z[8] = {L'▢', L'▢', L'_', L'/',
-						   L'_', L'▢', L'▢', L'*'};
+const wchar_t tetr_Z_0[7] = {L'▢', L'▢', L'/',
+							 L'_', L'▢', L'▢', L'*'};
+
+const wchar_t tetr_Z_1[8] = {L'_', L'▢', L'/',
+							 L'▢', L'▢', L'/',
+							 L'▢', L'*'};
 
 /**
  * _ # # /
- * # # _ *
+ * # # *
+ * 
+ * # /
+ * # # /
+ * _ # *
  */
-const wchar_t tetr_S[8] = {L'_', L'▧', L'▧', L'/',
-						   L'▧', L'▧', L'_', L'*'};
+const wchar_t tetr_S_0[7] = {L'_', L'▧', L'▧', L'/',
+							 L'▧', L'▧', L'*'};
 
-const wchar_t *const allTetraminos[NUM_TETRAMINOS] = {tetr_I, tetr_L, tetr_J, tetr_Q, tetr_Z, tetr_S, tetr_T};
+const wchar_t tetr_S_1[8] = {L'▧', L'/',
+							 L'▧', L'▧', L'/',
+							 L'_', L'▧', L'*'};
+
+const wchar_t *const allTetraminos[NUM_TETRAMINOS] = {tetr_I_0, tetr_L_0, tetr_J_0, tetr_Q_0, tetr_Z_0, tetr_S_0, tetr_T_0};
